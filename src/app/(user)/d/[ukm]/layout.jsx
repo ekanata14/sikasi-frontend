29cbed0a8827d839bcd420e9dc14ab88957295d2
Wrehaspati <gedereyna@gmail.com>
2024-10-13 13:24:11 +0800

    restructuring routing folder
    make it easier

--- src/app/(user)/d/[ukm]/layout.jsx
+++ src/app/(user)/d/[ukm]/layout.jsx
@@ -1,6 +1,17 @@
+import { Navbar } from "~/components/navigation-bar";
+import React from "react";
+import { cn } from "~/lib/utils";
+import { Backdrop } from "~/components/backdrop";
 import { Data } from "~/data/data";
 
-export default function Layout({ user, admin }) {
-  const role = Data.user.role;
-  return <>{role === "admin" ? admin : user}</>;
+export default function Layout({ children }) {
+  return(
+    <div className="lg:grid">
+      <div className={cn("w-full py-4 pb-32", "lg:px-8 lg:pb-0 lg:order-2 lg:mx-auto")}>
+        { children }
+      </div>
+      <Navbar className={"lg:order-1"} data={Data} />
+      {/* <Backdrop/> */}
+    </div>
+  ); 
 }
